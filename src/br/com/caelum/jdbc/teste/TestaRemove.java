1d5ca9d7d7d69b6680ced20fb96f3c02f802b14a
Fábio Ueno <fabio.t.ueno@gmail.com>
2016-08-01 14:01:55 -0300

    [Exercise 2.17] Optionals - Update & Remove
    Update and remove methods created along with its tests.

---
+++ src/br/com/caelum/jdbc/teste/TestaRemove.java
@@ -0,0 +1,16 @@
+package br.com.caelum.jdbc.teste;
+
+import br.com.caelum.jdbc.dao.ContatoDao;
+import br.com.caelum.jdbc.modelo.Contato;
+
+public class TestaRemove {
+
+	public static void main(String[] args) {
+		ContatoDao dao = new ContatoDao();
+
+		Contato contato = dao.pesquisar(1);
+		dao.remove(contato);
+
+		System.out.println("Removido!");
+	}
+}
